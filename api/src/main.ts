80e118019735db95804e6b078d9ff65ed2d14d9b
a.tamazyan <tamazyanarsen@focus.bi>
2020-10-29 09:09:58 +0300

    Удалён лишний код

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { NestMiddleware } from "@nestjs/common";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:4200',
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         preflightContinue: false,
         optionsSuccessStatus: 204,
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../environments/environment";
+import { checkIfClassIsExported } from "@angular/compiler-cli/src/ngtsc/typecheck/src/ts_util";
 
 @Injectable({
   providedIn: 'root'
