96c31bc928ccc52be112843fe60322e6efdb7084
a.tamazyan <tamazyanarsen@focus.bi>
2020-11-06 08:40:42 +0300

    отправляем токен на клиенте

--- client/src/app/core/auth.interceptor.ts
+++ client/src/app/core/auth.interceptor.ts
@@ -1,18 +1,12 @@
-import {
-    HttpErrorResponse,
-    HttpEvent,
-    HttpHandler,
-    HttpInterceptor,
-    HttpRequest,
-    HttpResponse
-} from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { tap } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor() {
+    constructor(private router: Router) {
     }
 
     intercept(
@@ -20,24 +14,30 @@ export class AuthInterceptor implements HttpInterceptor {
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
         // req.headers.set('Authorization', localStorage.getItem('accessToken'));
+        const token = localStorage.getItem('accessToken') || '';
         const authReq = req.clone({
             // headers: req.headers.set('Session', '123456789').set('Access-Control-Allow-Origin', '*'),
-            // headers: req.headers.set('Authorization', localStorage.getItem('accessToken'))
+            headers: req.headers.set('Authorization', token)
         });
 
         return next.handle(authReq).pipe(
             tap(
                 (event) => {
-                    if (event instanceof HttpResponse)
-                        console.log('Server response');
+                    if (event instanceof HttpResponse) {
+                        console.log('Server response', event);
+                        if (event.body.status === 401 || event.status === 401) {
+                            this.router.navigate(['/auth']);
+                        }
+                    }
                 },
                 (err) => {
                     if (err instanceof HttpErrorResponse) {
-                        if (err.status == 401)
+                        if (err.status === 401) {
                             console.log('Unauthorized');
+                        }
                     }
                 }
             )
-        )
+        );
     }
 }
