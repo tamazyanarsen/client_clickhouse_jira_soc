8360a5f52b544e17c322ef6f94b2d48572e69596
nikol <nikolaybondar97@gmail.com>
2020-11-24 09:06:30 +0300

    Бургер меню

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import {MatDrawer, MatDrawerContainer} from "@angular/material/sidenav";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
 
 @Component({
     selector: 'app-root',
